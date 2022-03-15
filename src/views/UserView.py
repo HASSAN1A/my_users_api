de2d06531e43383f39201060c258440f4321a287
HASSAN1A <okothhassanjuma@gmail.com>
2022-03-16 00:39:26 +0300

    Implement login for the app

--- src/views/UserView.py
+++ src/views/UserView.py
@@ -90,6 +90,26 @@ def get_me():
   return custom_response(ser_user, 200)
 
 
+@user_api.route('/login', methods=['POST'])
+def login():
+  """
+  User Login Function
+  """
+  req_data = request.get_json()
+
+  data, error = user_schema.load(req_data, partial=True)
+  if error:
+    return custom_response(error, 400)
+  if not data.get('email') or not data.get('password'):
+    return custom_response({'error': 'you need email and password to sign in'}, 400)
+  user = UserModel.get_user_by_email(data.get('email'))
+  if not user:
+    return custom_response({'error': 'invalid credentials'}, 400)
+  if not user.check_hash(data.get('password')):
+    return custom_response({'error': 'invalid credentials'}, 400)
+  ser_data = user_schema.dump(user).data
+  token = Auth.generate_token(ser_data.get('id'))
+  return custom_response({'jwt_token': token}, 200)
 
 def custom_response(res, status_code):
   """
