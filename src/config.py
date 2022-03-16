1b8182d1e2c7d974e68e1e422a09d1aa79a876ae
HASSAN1A <okothhassanjuma@gmail.com>
2022-03-16 16:56:27 +0300

    configure Redis caching

--- src/config.py
+++ src/config.py
@@ -14,6 +14,12 @@ class Development(object):
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    CACHE_TYPE = os.environ['CACHE_TYPE']
+    CACHE_REDIS_HOST = os.environ['CACHE_REDIS_HOST']
+    CACHE_REDIS_PORT = os.environ['CACHE_REDIS_PORT']
+    CACHE_REDIS_DB = os.environ['CACHE_REDIS_DB']
+    CACHE_REDIS_URL = os.environ['CACHE_REDIS_URL']
+    CACHE_DEFAULT_TIMEOUT = os.environ['CACHE_DEFAULT_TIMEOUT']
 
 class Production(object):
     """
@@ -24,6 +30,12 @@ class Production(object):
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    CACHE_TYPE = os.environ['CACHE_TYPE']
+    CACHE_REDIS_HOST = os.environ['CACHE_REDIS_HOST']
+    CACHE_REDIS_PORT = os.environ['CACHE_REDIS_PORT']
+    CACHE_REDIS_DB = os.environ['CACHE_REDIS_DB']
+    CACHE_REDIS_URL = os.environ['CACHE_REDIS_URL']
+    CACHE_DEFAULT_TIMEOUT = os.environ['CACHE_DEFAULT_TIMEOUT']
 
 class Testing(object):
     """
@@ -33,6 +45,12 @@ class Testing(object):
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_TEST_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
+    CACHE_TYPE = os.environ['CACHE_TYPE']
+    CACHE_REDIS_HOST = os.environ['CACHE_REDIS_HOST']
+    CACHE_REDIS_PORT = os.environ['CACHE_REDIS_PORT']
+    CACHE_REDIS_DB = os.environ['CACHE_REDIS_DB']
+    CACHE_REDIS_URL = os.environ['CACHE_REDIS_URL']
+    CACHE_DEFAULT_TIMEOUT = os.environ['CACHE_DEFAULT_TIMEOUT']
 
 app_config = {
     'development': Development,
