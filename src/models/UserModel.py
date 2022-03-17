74bc4635e847efe129ee62b5a2f873c8933a617b
HASSAN1A <okothhassanjuma@gmail.com>
2022-03-18 01:28:56 +0300

    Ferialize response and implement filter

--- src/models/UserModel.py
+++ src/models/UserModel.py
@@ -1,9 +1,10 @@
 # src/models/UserModel.py
 from marshmallow import fields, Schema
 import datetime
+from flask_login import login_manager, UserMixin
 from . import db, bcrypt
 
-class UserModel(db.Model):
+class UserModel(UserMixin, db.Model):
   """
   User Model
   """
@@ -32,10 +33,14 @@ class UserModel(db.Model):
   def update(self, data):
     for key, item in data.items():
       if key == 'password':
-        self.password = self.__generate_hash(value)
+        self.password = self.__generate_hash(item)
       setattr(self, key, item)
     self.modified_at = datetime.datetime.utcnow()
     db.session.commit()
+
+
+  def is_active():
+    return True
 
   def delete(self):
     db.session.delete(self)
@@ -45,6 +50,8 @@ class UserModel(db.Model):
   def get_all_users():
     return UserModel.query.all()
 
+  def get_id(self):
+    return str(self.id)
   @staticmethod
   def get_one_user(id):
     return UserModel.query.get(id)
@@ -53,19 +60,43 @@ class UserModel(db.Model):
   def get_user_by_email(value):
     return UserModel.query.filter_by(email=value).first()
 
+
+  @staticmethod
+  def filter_users_by_name(name):
+    qs = UserModel.query.filter_by(name=name).all()
+    return [item.serialize for item in qs]
+
+  @staticmethod
+  def filter_by_range(start, end):
+    qs = UserModel.query.offset(start).limit(end).all()
+    return [item.serialize for item in qs]
+    
   def __generate_hash(self, password):
     return bcrypt.generate_password_hash(password, rounds=10).decode("utf-8")
   
   def check_hash(self, password):
     return bcrypt.check_password_hash(self.password, password)
   
-  def __repr(self):
-    return '<id {}>'.format(self.id)
+  @property
+  def serialize(self):
+    '''Return a serialized response of the Model'''
+    context ={
+      "user_id": self.id,
+      "name": self.name,
+      "email": self.email,
+    }
+    return context
+  def __str__(self):
+    return self.name
+
 
 class UserSchema(Schema):
   id = fields.Int(dump_only=True)
   name = fields.Str(required=True)
   email = fields.Email(required=True)
   password = fields.Str(required=True, load_only=True)
+
+  def __str__(self) -> str:
+      return self.name
 
 
