482a0bc91eab79254e94b3959ca7179a69235e53
HASSAN1A <okothhassanjuma@gmail.com>
2022-03-18 01:03:26 +0300

    initialize Migrate and LoginManager

--- src/app.py
+++ src/app.py
@@ -1,40 +1,38 @@
 #src/app.py
 
-import requests
+import requests, os
 from flask import Flask, jsonify, request
+from flask_migrate import Migrate
 from flask_caching import Cache
+
+from src.models.UserModel import UserModel
 from .config import app_config
 from .models import db, bcrypt
+from flask_login import LoginManager
 
 # import user_api blueprint
 from .views.UserView import user_api as user_blueprint
 
+migrate = Migrate()
+login = LoginManager()
 
-
+@login.user_loader
+def load_user(user_id):
+    return UserModel.query.get(int(user_id))
+app = Flask(__name__)
 def create_app(env_name):
-  """
-  Create app
-  """
+    """
+    Create app
+    """
 
-  # app initiliazation
-  app = Flask(__name__)
+    app.config.from_object(app_config[env_name])
 
-  app.config.from_object(app_config['development'])
+    cache = Cache(app)
+    bcrypt.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+    app.register_blueprint(user_blueprint)
+    app.secret_key = os.getenv('SECRET_KEY')
 
-  cache = Cache(app)
-  # initializing bcrypt and db
-  bcrypt.init_app(app)
-  db.init_app(app)
-
-  app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')
-
-  # @app.route('/', methods=['GET'])
-  # @cache.cached(timeout=30, query_string=True)
-  # def index():
-  #   """
-  #   Flask app
-  #   """
-  #   return 'Hello world my users'
-
-  return app
-
+    return app
