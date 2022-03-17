57fcd1b43c0b5b15171b38a9a3edf84e30506b00
HASSAN1A <okothhassanjuma@gmail.com>
2022-03-18 01:23:51 +0300

    Created filter and point,logins and pagination

--- src/views/UserView.py
+++ src/views/UserView.py
@@ -1,13 +1,145 @@
 #/src/views/UserView
 
 from cgi import test
-from flask import request, json, Response, Blueprint, jsonify
+from email import message
+import json
+from multiprocessing import context
+from sre_constants import SUCCESS
+from flask import (
+    request, 
+    json, Response, Blueprint, 
+    jsonify, render_template, 
+    redirect, url_for, flash
+  )
+from itsdangerous import Serializer
+from flask_login import login_required, logout_user, login_user
+from src.models.forms import RegistrationForm, LoginForm
+from src.models import db
 from ..models.UserModel import UserModel, UserSchema
 from ..auth.Authentication import Auth
 
 user_api = Blueprint('user_api', __name__)
 user_schema = UserSchema()
 
+@user_api.route('/register', methods = ['GET', 'POST'])
+def user_registration():
+  '''User Registration View'''
+  form = RegistrationForm(request.form)
+  if request.method == 'POST' and form.validate():
+      data = request.form
+      user = UserModel(data)
+
+      user.save()
+      return redirect(url_for('user_api.login'))
+  return render_template('register.html',form = form)
+
+@user_api.route('/login', methods=['POST', 'GET'])
+def login():
+    """
+    User Login View
+    """
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+      # req_data = request.get_json()
+      req_data = json.dumps(request.form)
+      data = user_schema.load(json.loads(req_data), partial=True)
+      
+      if not data.get('email') or not data.get('password'):
+        return custom_response({'error': 'you need email and password to sign in'}, 400)
+      user = UserModel.get_user_by_email(data.get('email'))
+
+      if not user:
+        return custom_response({'error': 'invalid credentials'}, 400)
+
+      if not user.check_hash(data.get('password')):
+        return custom_response({'error': 'invalid credentials'}, 400)
+
+      login_user(user)
+      flash({"Login Success": "Kabisa", 'code': 200})
+      return redirect(url_for('user_api.index'))
+      
+    return render_template('login.html', form =form)
+
+@user_api.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for("user_api.index"))
+@user_api.route('/users/', defaults = {'n':None})
+@user_api.route('/users/<int:n>/')
+@login_required
+def get_users(n):
+  '''This function returns a paginated number of results, the default is 100'''
+  global ROWS_PER_PAGE
+  if n is not None:
+    ROWS_PER_PAGE = int(n)
+  else:
+    ROWS_PER_PAGE = 100
+  page = request.args.get('page', 1, type=int)
+  qs = UserModel.query.paginate(page = page, per_page = ROWS_PER_PAGE)
+  qs = qs.items
+  serializer =  [user.serialize for user in qs]
+  context ={
+    'count': len(qs),
+    "message": "SUCCESS",
+    'status_code': 200,
+    'users': serializer
+    }
+  response = jsonify(context)
+  return response
+
+@user_api.route('/users/range/', defaults = {"start":0, "end": 100} )
+@user_api.route('/users/range/<int:start>/<int:end>/')
+@login_required
+def filter_by_range(start, end):
+  '''This function queries users by the id range provided.
+    Params: start+1 => integer
+            end => integer, the limit of results
+            Example:
+            /users/range/<5/2/ => Will return a query from id 6 to 7
+
+  '''
+  serializer = UserModel.filter_by_range(start=start, end=end)
+  context = {
+    'count': len(serializer),
+    "message": "SUCCESS",
+    'status_code': 200,
+    'users': serializer
+  }
+  return jsonify(context)
+
+
+
+@user_api.route('/user/<int:val>/')
+@login_required
+def get_user(val):
+  '''This function returns a filter of a single user by their id or email'''
+  if val.isdigit():
+    context = {
+      "status code": 200,
+      "message": 'SUCCESS',
+      "response":UserModel.get_one_user(val).serialize
+    }
+    return jsonify(context)
+  if "@" in val:
+    context = {
+      "status code": 200,
+      "message": 'SUCCESS',
+      "response":UserModel.get_user_by_email(val).serialize
+    }
+    return jsonify(context)
+
+@user_api.route('/users/<name>/')
+@login_required
+def filter_by_name(name):
+  '''This function returns a filter of users by name'''
+  serializer = UserModel.filter_users_by_name(name)
+  context = {
+    'count': len(serializer),
+    "message": "SUCCESS",
+    'status_code': 200,
+    'users': serializer
+  }
+  return jsonify(context)
 @user_api.route('/', methods=['POST'])
 def create():
   """
@@ -32,27 +164,12 @@ def create():
   return custom_response({'jwt_token': token}, 201)
 
 @user_api.route('/', methods=['GET'])
-@Auth.auth_required
-def get_all():
+@login_required
+def index():
   """
   Get all users
   """
-  users = UserModel.get_all_users()
-  ser_users = user_schema.dump(users, many=True).data
-  return custom_response(ser_users, 200)
-
-@user_api.route('/<int:user_id>', methods=['GET'])
-@Auth.auth_required
-def get_a_user(user_id):
-  """
-  Get a single user
-  """
-  user = UserModel.get_one_user(user_id)
-  if not user:
-    return custom_response({'error': 'user not found'}, 404)
-  
-  ser_user = user_schema.dump(user).data
-  return custom_response(ser_user, 200)
+  return render_template('index.html')
 
 @user_api.route('/me', methods=['PUT'])
 @Auth.auth_required
@@ -89,36 +206,13 @@ def get_me():
   user = UserModel.get_one_user(g.user.get('id'))
   ser_user = user_schema.dump(user).data
   return custom_response(ser_user, 200)
-
-
-@user_api.route('/login', methods=['POST'])
-def login():
-  """
-  User Login Function
-  """
-  req_data = request.get_json()
-
-  data, error = user_schema.load(req_data, partial=True)
-  if error:
-    return custom_response(error, 400)
-  if not data.get('email') or not data.get('password'):
-    return custom_response({'error': 'you need email and password to sign in'}, 400)
-  user = UserModel.get_user_by_email(data.get('email'))
-  if not user:
-    return custom_response({'error': 'invalid credentials'}, 400)
-  if not user.check_hash(data.get('password')):
-    return custom_response({'error': 'invalid credentials'}, 400)
-  ser_data = user_schema.dump(user).data
-  token = Auth.generate_token(ser_data.get('id'))
-  return custom_response({'jwt_token': token}, 200)
-
+ 
 def custom_response(res, status_code):
   """
   Custom Response Function
   """
   return Response(
     mimetype="application/json",
-    response=json.dumps(res),
+    response=jsonify(res),
     status=status_code
   )
-
